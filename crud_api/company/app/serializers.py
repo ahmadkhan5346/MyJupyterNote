045d0ff46de2b77926a0cba8d64b54a85e69fa4a
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-07-22 12:44:53 +0530

    practice on data structrue

--- crud_api/company/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.models import Contact
-
-
-class ContactApiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ["id", "name", "roll_no", "city"]
--- crud_api/company/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app.views import StudentApi
-
-
-
-
-urlpatterns = [
-    path('student/', StudentApi.as_view(), name='student'),
-]
